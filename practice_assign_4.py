69fda16231c6ff59ec4b1b5895e444f8f6d3fd32
amandwivedi70 <111753149+amandwivedi70@users.noreply.github.com>
2022-09-14 13:08:14 +0530

    Add files via upload

---
+++ practice_assign_4.py
@@ -0,0 +1,67 @@
+# question 1
+name = input("enter your name")
+print(name)
+
+# question 2
+number = int(input("enter number"))
+print(number)
+
+# question 3
+
+a = int(input("enter a number1"))
+b = int(input("enter a number2"))
+c = (a+b)
+print("sum of two number is",(c))
+
+# question 4
+PI_value = 3.14
+radius = float(input("enter radius"))
+area_of_circle = PI_value * radius* radius
+print(area_of_circle)
+
+# question 5
+
+area = float(input("enter side"))
+area_of_square = area* area
+print(area_of_square)
+
+# question 6
+
+area = float(input( ("enter length and height"))
+
+# question 7
+
+number1 = float(input("enter number"))
+number2 = float(input("enter number"))
+number3 = float(input("enter number"))
+
+average_of_numbers = (number1 + number2 + number3) / 3
+print(average_of_numbers)
+
+# question 8
+
+p = float(input("enter number"))
+t = float(input("enter number"))
+r = float(input("enter number"))
+
+simple_interest = (p * t * r) / 100
+print(simple_interest)
+
+# question 9
+
+lenth = float(input("enter number"))
+width = float(input("enter number"))
+height = float(input("enter number"))
+
+volume_of_cuboid = (length * width * height)
+print(volume_of_cuboid )
+
+# question 10
+
+length = float(input("enter number"))
+
+breadth = float(input("enter number"))
+
+area_of_rectangle = (length * breadth)
+
+print(area_of_rectangle)
